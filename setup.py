636efa7071a12933ab0e1de1ba33cf6435c4be09
Andrew <andrew@philippick.com>
2022-12-04 15:45:36 +0100

    init commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lib_name",
+    version="0.1.0",
+    description="Example of an internal Python library that can be used within a Data Team.",
+    author="Andrew P",
+    install_requires=[
+        "slacker"    
+    ],
+    packages=find_packages()
+)
+
